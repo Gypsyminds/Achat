2b6b7173dd3b2f22a92b15446003f82beef906f1
Gypsyminds <khouloud.laajili12@gmail.com>
2022-11-06 14:44:33 +0100

    pomm1

---
+++ src/test/java/tn/esprit/rh/achat/StockTest.java
@@ -0,0 +1,17 @@
+package tn.esprit.rh.achat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.IStockService;
+
+
+public class StockTest {
+
+  @Test
+    void test ()
+  {}
+}
