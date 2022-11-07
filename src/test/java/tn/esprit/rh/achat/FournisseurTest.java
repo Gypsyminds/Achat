b0a7440a353a7f89ee2bd234d1f52d63e324fec1
Gypsyminds <khouloud.laajili12@gmail.com>
2022-11-07 11:09:41 +0100

    test4

--- src/test/java/tn/esprit/rh/achat/FournisseurTest.java
+++ src/test/java/tn/esprit/rh/achat/FournisseurTest.java
@@ -8,33 +8,28 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import tn.esprit.rh.achat.entities.DetailFournisseur;
+
 import tn.esprit.rh.achat.entities.Fournisseur;
-import tn.esprit.rh.achat.repositories.DetailFournisseurRepository;
+
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
 import tn.esprit.rh.achat.services.FournisseurServiceImpl;
-import tn.esprit.rh.achat.services.IFournisseurService;
 
-import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
-import static tn.esprit.rh.achat.entities.CategorieFournisseur.CONVENTIONNE;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class FournisseurTest {
 
     @Mock
-    FournisseurRepository rep ;
+    private  FournisseurRepository rep ;
     @InjectMocks
-    FournisseurServiceImpl service ;
-    @Mock
-    DetailFournisseurRepository detail ;
+    private FournisseurServiceImpl service ;
 
 
-    private static final Logger l = LogManager.getLogger(AchatDevopsTest.class);
+
+    //private static final Logger l = LogManager.getLogger(AchatDevopsTest.class);
 
     @Test
     public void testAjouterFournisseur() {
@@ -43,7 +38,7 @@ public class FournisseurTest {
         Fournisseur fournisseur = new Fournisseur();
         //when
         service.addFournisseur(fournisseur);
-        ;
+
         //then
         ArgumentCaptor<Fournisseur> saArgumentCaptor = ArgumentCaptor.forClass(Fournisseur.class);
         verify(rep).save(saArgumentCaptor.capture());
@@ -67,15 +62,7 @@ public class FournisseurTest {
 
     }
 
-        @Test
-        public void updateOperateur() {
-        Fournisseur sa = new Fournisseur();
-        sa.setCode("test");
-        Fournisseur sa1 = service.updateFournisseur(sa) ;
-
-        assertThat(sa1.getCode()).isEqualTo("test") ;
-
-    }
+      
 
     }
 
