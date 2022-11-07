a0fb7ff03dbec6881ad1353809b2ea5f98eb0f8d
Gypsyminds <khouloud.laajili12@gmail.com>
2022-11-07 13:46:13 +0100

    test4

--- src/test/java/tn/esprit/rh/achat/FournisseurTest.java
+++ src/test/java/tn/esprit/rh/achat/FournisseurTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Fournisseur;
 
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
@@ -22,11 +23,12 @@ import static org.mockito.Mockito.verify;
 
 
 @RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
 public class FournisseurTest {
 
     @Mock
     private  FournisseurRepository rep ;
-    @InjectMocks
+    @Mock
     private FournisseurServiceImpl service ;
 
 
@@ -36,30 +38,29 @@ public class FournisseurTest {
     @Test
     public void testAjouterFournisseur() {
         try {
-            l.info("In testAjouterEntreprise():");
-            Fournisseur f =new Fournisseur("testAjout", "testAjout");
-            ArrayList<Fournisseur> liste1 =(ArrayList<Fournisseur>) rep.findAll();
+            l.info("In testAjouterfournisseur():");
+            Fournisseur fournisseur=new Fournisseur("testAjout", "testAjout");
+            ArrayList <Fournisseur> liste1 =(ArrayList<Fournisseur>) rep.findAll();
             int size1=liste1.size();
-            l.info(()->"nombre d'entreprises avant l'ajout: " +size1);
-            l.info("Je vais ajouter une entreprise.");
-            int id=service.addFournisseur(f);
+
+            l.info(()->"nombre d'fournisseur avant l'ajout: " +size1);
+            l.info("Je vais ajouter une fournisseur.");
+            int id=service.addFournisseur(fournisseur);
             ArrayList <Fournisseur> liste2 =(ArrayList<Fournisseur>) rep.findAll();
+
             int size2=liste2.size();
             l.info(()->"nombre d'entreprises apres l'ajout: " +size2);
             l.info("comparaison size avant et apres.");
 
 
 
-            service.deleteFournisseur((long) id);
-            l.info("je supprime l'entreprise.");
-            l.info("Out testAjouterEntreprise() sans erreurs.");
+           // service.deleteFournisseur((long) id);
+           // l.info("je supprime l'entreprise.");
+            //l.info("Out testAjouterEntreprise() sans erreurs.");
         }
         catch (Exception e)
         { l.error(()->"Erreur dans testAjouterEntreprise() : " + e); }
     }
-
-
-
 
     }
 
